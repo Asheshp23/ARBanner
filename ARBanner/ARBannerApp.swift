da8dd0f30c4d4544810a4518c6a510d578cdc9c7
asheshp23 <asheshp23@gmail.com>
2025-06-23 15:21:01 -0400

    Initial Commit

---
+++ ARBanner/ARBannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARBannerApp.swift
+//  ARBanner
+//
+//  Created by Ashesh Patel on 2025-06-23.
+//
+
+import SwiftUI
+
+@main
+struct ARBannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARBanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARBanner
+//
+//  Created by Ashesh Patel on 2025-06-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
